7fb8f99f030094983026d07d3cc2ae602afb7190
ruibo <robertsnoeberger@Roberts-MacBook-Pro-2.local>
2014-06-17 21:47:57 -0400

    Adding setup.py file for install.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+
+setup(
+    name='scmimport',
+    version='0.1',
+    description='import Scheme code.',
+    author='Lee Rui-bo',
+    url='http://github.com/ruibo/scm-import-utils',
+    packages=['scmimport'],
+    )
